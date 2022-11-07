0fb3078243c321748bb52ad0795f37da1f8e7dde
Paul Kasidit <paulkasidit@gmail.com>
2022-11-06 16:00:41 -0800

    create fake db seeds

--- app/controllers/jackets_controller.rb
+++ app/controllers/jackets_controller.rb
@@ -1,12 +1,36 @@
 class JacketsController < ApplicationController
 
-  def index
-    @quotes = {"jacket-brand": "Gocci"}
-    json_response(@quotes)
-  end
-
-  private
-  def json_response(object, status = :ok)
-    render json: object, status: status
-  end
+    def index
+      @jackets = Jacket.all
+      json_response(@jackets)
+    end
+  
+    def show
+      @jacket = Jacket.find(params[:id])
+      json_response(@jacket)
+    end
+  
+    def create
+      @jacket = Jacket.create(jacket_params)
+      json_response(@jacket)
+    end
+  
+    def update
+      @jacket = Jacket.find(params[:id])
+      @jacket.update(jacket_params)
+    end
+  
+    def destroy
+      @jacket = Jacket.find(params[:id])
+      @jacket.destroy
+    end
+  
+    private
+    def json_response(object, status = :ok)
+      render json: object, status: status
+    end
+  
+    def jacket_params
+      params.permit(:jacket_brand, :jacket_manufacturer, :jacket_category)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+class Seed 
+
+  def self.begin
+    seed = Seed.new 
+    seed.generate_jackets 
+  end
+
+  def generate_jackets 
+    50.times do |i|
+
+      jacket_factories = [
+        "Shenzhen Wanhao Mfg. Co., Ltd.",
+        "Guangdong Textiles Imp. & Exp. Co., Ltd.",
+        "Zhuhai Nazhou Buliao Cheng",
+        "Lida Textile (Zhuhai) Ltd.",
+        "Guangzhou Quande Co., Ltd."
+      ]
+      
+      jacket_brands = [
+        "Gocci",
+        "Louis Button",
+        "Moncat",
+        "Ballengciaga",
+        "Hemes",
+        "Noke",
+        "Cumin Projects",
+        "Adidadas",
+        "Superb",
+        "Wanhao",
+        "Shenzhenfuzhuangwang",
+        "Gocci x Adidadas",
+        "Canadein Goose"
+      ]
+      
+      jacket_categories = ["bomber",
+      "trench",
+      "puffer"
+      ]
+      
+      jacket = Jacket.create!(
+        jacket_brand: (jacket_brands).sample,
+        jacket_manufacturer: (jacket_factories).sample,
+        jacket_category: (jacket_categories).sample
+      )
+      puts "Jacket #{i}: Manufacturer is #{jacket.jacket_manufacturer}, jacket brand is #{jacket.jacket_brand}, and 
+      category is '#{jacket.jacket_category}'"
+    end
+  end
+
+end
+
+Seed.begin
