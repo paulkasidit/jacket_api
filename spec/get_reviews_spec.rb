70fe91e59307f145eb9c05ecd18f111a59a7c651
Paul Kasidit <paulkasidit@gmail.com>
2022-11-19 02:56:12 -0800

    create controllers for reviews

--- spec/get_reviews_spec.rb
+++ spec/get_reviews_spec.rb
@@ -1,1 +1,25 @@
 require 'rails_helper' 
+
+describe "get reviews based on jacket id", :type => :request do 
+
+  
+  before do  
+    post '/jackets/1', params: {:jacket_brand => 'Moncler',
+      :jacket_manufacturer => 'French Company',
+      :jacket_category => 'bomber' }  
+    post 'jackets/1/reviews',   params: { :jacket_id => 1, 
+                                :author => 'Paul'
+                                :content_body => 'The best jacket ever, the fabric is soft to the touch.'}
+    get 'jackets/1/reviews'
+  end 
+
+  it 'returns all associated reviews witht the jacket' do
+    expect(JSON.parse(response.body).size).to eq(1)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(200)
+  end
+
+end 
+
