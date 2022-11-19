b00b2cdb8592bc75addfc2cbbd7ab8287730b781
Paul Kasidit <paulkasidit@gmail.com>
2022-11-19 02:22:56 -0800

    nest review routes in jackets

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :jackets
+  resources :jackets do
+    resources :reviews
+  end
+  
 end
